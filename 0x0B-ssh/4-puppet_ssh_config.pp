85f15a12abdc29923f6d7936c6f1d0a40d967757
zinczar <brendan@wineisneat.com>
2019-12-11 00:39:06 +0000

    #4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,11 @@
-#
+#set up client SSH configuration file so you can connect with no password
+
+file_line {'Setting Up Identity File':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line {'Setting Up No Password':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
